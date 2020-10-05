0015a17428718c2e0c23cb70289faeed757b44a2
Leandro Mancini de Sousa <mancinilele@gmail.com>
2020-10-05 16:02:38 -0300

    teste

--- src/FFModule.ts
+++ src/FFModule.ts
@@ -4,6 +4,8 @@ import { Observable, of } from 'rxjs';
 import { ApiFeature } from './ApiFeature';
 import { FFConfig } from './FFConfig';
 import { DEVICE } from './Globals';
+import { map } from 'rxjs/operators';
+import { Feature } from './models/Feature';
 
 export class FFModule {
   // feature: Feature;
@@ -35,9 +37,21 @@ export class FFModule {
     return apiFeature.teste();
   }
 
-  testeGetFeature(featureName: string): Observable<string> {
+  testeGetFeature(featureName: string): Observable<any> {
     const apiFeature = new ApiFeature(this.config.url, this.config.device);
 
-    return apiFeature.teste(featureName);
+    return apiFeature.teste(featureName).pipe(
+      map((item: Feature) => this.testeTransformType(item))
+    );
+  }
+
+  testeTransformType(item: Feature) {
+    let type;
+
+    if (item.type === "B") {
+      type = JSON.parse(item.type);
+    }
+
+    return type;
   }
 }
